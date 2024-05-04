56b45a46e8c2684ac9b54bc705a8588b07cbf110
Grace <graceodonnell2013@gmail.com>
2024-05-04 13:47:26 +0100

    Store display first digit

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
-subtract;
-multiply;
-divide;
+let result = 0;
+let display = document.querySelector("p");
+console.log(display);
+display.textContent = result;
 
-function add(a, b) {
-  return a + b;
+let buttons = document.querySelectorAll("button");
+
+//function storeNumber() {
+// let btn = buttons.forEach((button) => button.id);
+// console.log(btn);
+//display.textContent = btn;
+//}
+
+const buttonPressed = (e) => {
+  console.log(e.target.id); // Get ID of Clicked Element
+};
+
+for (let button of buttons) {
+  button.addEventListener("click", buttonPressed);
 }
 
-function subtract(a, b) {
-  return a - b;
+function add(result, b) {
+  return result + b;
 }
 
-function multiply(a, b) {
-  return a * b;
+function subtract(result, b) {
+  return result - b;
 }
 
-function divide(a, b) {
-  return a / b;
+function multiply(result, b) {
+  return result * b;
 }
 
-let a;
+function divide(result, b) {
+  return result / b;
+}
+
 let b;
 let op;
+//function operate (a,b, op, function) {
+
+//  if (op === "-") { function subtract(a,b)};
+//   else (op === "+") { function subtract(a,b)
+//}
